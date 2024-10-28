9a7ba2c07c7ec6fd9b6893148210082bc5d3f4ed
Victor Okumu <149936541+BitBender4u@users.noreply.github.com>
2024-10-28 14:18:03 +0300

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -1,9 +1,12 @@
-for i in range(1,50):
-  if i%3==0 :
-    print("Fizz")
-  elif i%5==0:
-    print("Buzz")
-  if i%3==0 and i%5==0:
-    print("FizzBuzz")
-  else:
-    print(i)
+def fun():
+  for i in range(1,50):
+    if i%3==0 :
+      print("Fizz")
+    elif i%5==0:
+      print("Buzz")
+    if i%3==0 and i%5==0:
+      print("FizzBuzz")
+    else:
+      print(i)
+
+fun()
